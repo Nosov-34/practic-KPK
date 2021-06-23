9e19cab413b8e69e389b3762a31afcdfb5ea24a1
KPK_2 <KPK@mail.ru>
2021-06-23 12:38:41 +0500

    1

---
+++ practic-KPK/Pages/Page5.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace practic_KPK.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Page5 : ContentPage
+{
+    public Page5()
+    {
+        InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
